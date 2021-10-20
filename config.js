db3b27bb8aa1fb0a51090dce8c7595a6b27c9522
Lequan13012000 <82744257+Lequan13012000@users.noreply.github.com>
2021-10-20 22:14:15 +0700

    Update config.js

--- config.js
+++ config.js
@@ -8,6 +8,6 @@ const contentCard = 'Món quà cuối cùng cho bé Quỳnh hum nay <3';
 // Cần hỗ trợ hãy liên hệ: 
 // Mr-Nam http://facebook.com/nam.nodemy
 // Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
-const giftImage = 'hot-girl.png';
+const giftImage = 'diemquynh.png';
 const base64 = '';
 const giftImageBase64 = "data:image/png;base64, " + base64;
