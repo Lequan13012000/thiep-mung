1c24d0e77e02d7d269ed170e724ffa61c7374070
Lequan13012000 <82744257+Lequan13012000@users.noreply.github.com>
2021-10-20 22:04:11 +0700

    Update present.js

--- present.js
+++ present.js
@@ -4,7 +4,7 @@
 // Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
 
 var to = nameGirl;
-var gift_url = giftUrl;
+// var gift_url = giftUrl;
 var gift_image_url = giftImage || giftImageBase64;
 
 var nametag = document.getElementById("nametag");
@@ -21,22 +21,22 @@ function init() {
   var _giftLink, 
       _giftImg;
   
-  if (gift_url) {
-    _giftLink = document.createElement("a");
-    _giftLink.href = gift_url;
-    _giftLink.target = "_blank";
-    presentImage.appendChild(_giftLink);
-  }
+//   if (gift_url) {
+//     _giftLink = document.createElement("a");
+//     _giftLink.href = gift_url;
+//     _giftLink.target = "_blank";
+//     presentImage.appendChild(_giftLink);
+//   }
   
-  if (gift_image_url) {
-    _giftImg = document.createElement("img");
-    _giftImg.src = gift_image_url;
-    if(_giftLink) {
-      _giftLink.appendChild(_giftImg);
-    } else {
-      presentImage.appendChild(_giftImg);
-    }
-  }
+//   if (gift_image_url) {
+//     _giftImg = document.createElement("img");
+//     _giftImg.src = gift_image_url;
+//     if(_giftLink) {
+//       _giftLink.appendChild(_giftImg);
+//     } else {
+//       presentImage.appendChild(_giftImg);
+//     }
+//   }
     
   present.addEventListener("click", function(e){
     present.classList.toggle("open");
